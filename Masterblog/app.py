a672351f9f5a9c5f16e129d3e0b047e127b4a873
unknown <nicolasheyer@gmail.com>
2025-05-30 19:06:40 +0200

    Adding templates for dynamic rendering

--- Masterblog/app.py
+++ Masterblog/app.py
@@ -1,11 +1,40 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+import json
+import os
 
 app = Flask(__name__)
+POSTS_FILE = 'posts.json'
 
+def load_posts():
+    if os.path.exists(POSTS_FILE):
+        with open(POSTS_FILE, 'r') as f:
+            return json.load(f)
+    return []
+
+def save_posts(posts):
+    with open(POSTS_FILE, 'w') as f:
+        json.dump(posts, f, indent=4)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    posts = load_posts()
+    return render_template('index.html', posts=posts)
+
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+        posts = load_posts()
+        new_post = {
+            "id": posts[-1]["id"] + 1 if posts else 1,
+            "author": request.form.get('author'),
+            "title": request.form.get('title'),
+            "content": request.form.get('content')
+        }
+        posts.append(new_post)
+        save_posts(posts)
+        return redirect(url_for('index'))
+
+    return render_template('add.html')
 
 
 if __name__ == '__main__':
